51f8d0c98090ac6d0751a890043831f63a66e2e4
joyadeep <dev.joyadeep@gmail.com>
2026-08-05 15:47:05 +0545

    footer links underline on hover

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,35 +4,97 @@ import MailIcon from "./icons/mailIcon"
 import PinterestIcon from "./icons/pinterestIcon"
 import TiktokIcon from "./icons/tiktokIcon"
 
+const shoplinks = [
+    {
+        title: "new in",
+        link:"#"
+    },
+    {
+        title: "best sellers",
+        link:"#"
+    },
+    {
+        title: "clothing",
+        link:"#"
+    },
+    {
+        title: "bags",
+        link:"#"
+    },
+    {
+        title: "accessories",
+        link:"#"
+    },
+    {
+        title: "sale",
+        link:"#"
+    },
+]
+
+const infoLinks = [
+    {
+        title: "shipping + returns",
+        link:"#"
+    },
+    {
+        title: "sizing",
+        link:"#"
+    },
+    {
+        title: "faq",
+        link:"#"
+    },
+    {
+        title: "about us",
+        link:"/about"
+    },
+    {
+        title: "journal",
+        link:"#"
+    }
+]
+
+const accountLinks = [
+    {
+        title: "account",
+        link:"#"
+    },
+    {
+        title: "orders",
+        link:"#"
+    },
+    {
+        title: "wishlist",
+        link:"#"
+    }
+]
+
+
+
 const Footer = () => {
   return (
     <footer className=" py-5 bg-pink-200 text-green-800">
-        {/* <div className='w-full max-w-5xl mx-auto flex justify-between items-center gap-16'> */}
         <div className='w-full max-w-400 px-5 mx-auto grid grid-cols-1 md:grid-cols-[1fr_2fr] lg:grid-cols-[1fr_2fr_1fr] items-center gap-5 '>
             <div className="font-dancing-script text-center md:text-left text-3xl -rotate-12 w-fit ">Thank you for <br /> being part of our <br /> little paradise</div>
             <div className="flex justify-between grow uppercase text-sm gap-5">
                 <div className="flex flex-1 flex-col gap-1 ">
                     <h6 className="text-base mb-2">shop</h6>
-                    <Link href={"#"} >new in</Link>
-                    <Link href={"#"} >best sellers</Link>
-                    <Link href={"#"} >clothing</Link>
-                    <Link href={"#"} >bags</Link>
-                    <Link href={"#"} >accessories</Link>
-                    <Link href={"#"} >sale</Link>
+                    {shoplinks.map((link)=>(
+                        <Link href={link.link} key={link.title} className="hover:underline" >{link.title}</Link>
+                    ))}
+                    
                 </div>
                 <div className="flex flex-1 flex-col gap-1 ">
                     <h6 className="text-base mb-2">info</h6>
-                    <Link href={"#"} >shipping + returns</Link>
-                    <Link href={"#"} >sizing</Link>
-                    <Link href={"#"} >faqs</Link>
-                    <Link href={"#"} >about us</Link>
-                    <Link href={"#"} >journal</Link>
+                    {infoLinks.map((link)=>(
+                        <Link href={link.link} key={link.title} className="hover:underline" >{link.title}</Link>
+                    ))}
                 </div>
                 <div className="flex flex-1 flex-col gap-1 ">
                     <h6 className="text-base mb-2">account</h6>
-                    <Link href={"#"} >my account</Link>
-                    <Link href={"#"} >order tracking</Link>
-                    <Link href={"#"} >wishlist</Link>
+                    {accountLinks.map((link)=>(
+                        <Link href={link.link} key={link.title} className="hover:underline" >{link.title}</Link>
+                    ))}
                 </div>
             </div>
             <div className="flex flex-col gap-2 md:col-span-2 lg:col-span-1 text-center text-green-900">
